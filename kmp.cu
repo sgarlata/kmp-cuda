542dc831bb83c7625f08b089137c303817b43b82
sgarlata <antonio.sgarlata11@gmail.com>
2021-11-03 01:08:37 +0100

    Polished the code

--- kmp.cu
+++ kmp.cu
@@ -1,11 +1,10 @@
 /*
 CUDA-BASED IMPLEMENTATION OF THE KMP ALGORITHM
-VERSION: USING ALSO THE GPU (NOT POLISHED)
+VERSION: PARTIALLY POLISHED
 */
 
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 #include <assert.h>
 
 #define MAX 50
@@ -42,39 +41,31 @@ __global__ void patternMatch(char *pattern, char *text, int *next, int m, int n)
   int j; // current position in pattern
   int k; // current position in text
   int idx = threadIdx.x + blockIdx.x * blockDim.x;
-  int sublength = ceilf( (float) n / (gridDim.x * blockDim.x));
-  int start = idx * sublength;
-  int stop = start + sublength;
+  int sublength = ceilf( (float) n / (gridDim.x * blockDim.x)); // text characters divided by the number of threads
+  int start = idx * sublength; // initial delimiter for each thread (included)
+  int stop = start + sublength; // final delimiter for each thread (excluded)
   int proceed = 1;
 
-  printf("Thread: %d. Start: %d. Stop: %d.\n", idx, start, stop);
-
   for (j = 0, k = start; j < m && k < n && proceed; ++j, ++k) {
-    //printf("Thread: %d. Inside for with j = %d and k = %d\n", idx, j, k);
     while (j >= 0 && text[k] != pattern[j]) {
-      //printf("Thread: %d. Inside while with j = %d and k = %d\n", idx, j, k);
       j = next[j];
-      //printf("Thread: %d. j is now %d\n", idx, j);
-      if (k - j >= stop) {
-      //printf("Thread: %d. Inside if with k = %d.\n", idx, k);
-        proceed = 0;
-        }
+      if (k - j >= stop)
+        proceed = 0; // a match should only be found by a single thread, namely the one whose text's portion contains the initial matched character
     }
   }
 
-    if (j == m) {
-      match = 1;
-      printf("Match found by thread n. %d in positions %d through %d.\n", idx, k - m, k - 1);
-    }
+  if (j == m) {
+    match = 1;
+    printf("Match found in positions %d through %d.\n", k - m, k - 1); // found matches are not necessarily printed in order
+  }
 }
 
 int kmp(char *text, char *pattern, int N, int M) {
-  int *next;
-  size_t next_size = M * sizeof(int);
+  int *next; // auxiliary array to know how far to slide the pattern when a mismatch is detected
 
-  checkCuda(cudaMallocManaged(&next, next_size));
+  checkCuda(cudaMallocManaged(&next, M * sizeof(int)));
   computeNext(next, pattern, M);
-
+  
   size_t threads_per_block = 4;
   size_t number_of_blocks = 2;
 
@@ -94,16 +85,14 @@ int main() {
   fgets(buffer, MAX+1, stdin);
   buffer[strcspn(buffer, "\n")] = 0; // to remove \n
   N = strlen(buffer);
-  size_t text_size = (N + 1) * sizeof(char);
-  checkCuda(cudaMallocManaged(&text, text_size));
+  checkCuda(cudaMallocManaged(&text, (N + 1) * sizeof(char)));
   strncpy(text, buffer, N);
 
   printf("Enter pattern: (at most %d characters): ", MAX);
   fgets(buffer, MAX+1, stdin);
   buffer[strcspn(buffer, "\n")] = 0; // to remove \n
   M = strlen(buffer);
-  size_t pattern_size = (M + 1) * sizeof(char);
-  checkCuda(cudaMallocManaged(&pattern, pattern_size));
+  checkCuda(cudaMallocManaged(&pattern, (M + 1) * sizeof(char)));
   strncpy(pattern, buffer, M);  
 
   if (!kmp(text, pattern, N, M))
